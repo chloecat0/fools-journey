8f17030b901fd2eecc7280c8d9d7a4bc856a8943
chloecat <43815120+chloecat0@users.noreply.github.com>
2026-05-15 22:35:06 +1200

    unbelievably basic weapon system

--- player.gd
+++ player.gd
@@ -4,22 +4,29 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var weapon : Node2D = preload("res://sword.tscn").instantiate()
+
+func _ready() -> void:
+	add_child(weapon)
 
 func _physics_process(delta: float) -> void:
+	weapon.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("attack"):
+		weapon.rotate(PI/4)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
